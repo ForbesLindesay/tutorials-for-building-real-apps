b032f2a1e28e98e2e9428c1fc471743526f36ca1
ForbesLindesay <forbes@lindesay.co.uk>
2015-11-05 19:31:28 +0000

    05

--- 05-list/sollution/index.js
+++ 05-list/sollution/index.js
@@ -3,33 +3,92 @@ var ReactDOM = require('react-dom');
 
 var container = document.getElementById('container');
 
-var ListOfNumbers = React.createClass({
-  displayName: 'ListOfNumbers',
+var NewItemForm = React.createClass({
+  displayName: 'NewItemForm',
 
   propTypes: {
-    max: React.PropTypes.number.isRequired,
+    onAdd: React.PropTypes.func.isRequired
+  },
+
+  getInitialState: function () {
+    return {
+      name: ''
+    };
+  },
+
+  _onChange: function (e) {
+    this.setState({
+      name: e.target.value
+    });
+  },
+
+  _onSubmit: function (e) {
+    // by defualt, the form will be sent to the server
+    // but we want to handle it in JavaScript
+    e.preventDefault();
+    this.props.onAdd(this.state.name);
+    this.setState({name: ''});
   },
 
   render: function () {
-    var numbers = [];
-    for (var i = 0; i < this.props.max; i++) {
-      numbers.push(i);
-    }
     return React.createElement(
-      'ul',
+      'form',
+      {onSubmit: this._onSubmit},
+      React.createElement(
+        'input',
+        {
+          value: this.state.name,
+          onChange: this._onChange
+        }
+      ),
+      React.createElement(
+        'button',
+        {type: 'submit'},
+        'Add'
+      )
+    );
+  }
+});
+
+var List = React.createClass({
+  displayName: 'List',
+
+  getInitialState: function () {
+    return {
+      items: []
+    };
+  },
+
+  _onAdd: function (item) {
+    this.setState({
+      items: this.state.items.concat([item])
+    });
+  },
+  
+  render: function () {
+    return React.createElement(
+      'div',
       {},
-      numbers.map(function (number) {
-        return React.createElement(
-          'li',
-          {key: number},
-          number
-        );
-      })
+      React.createElement(
+        'ul',
+        {},
+        this.state.items.map(function (item, i) {
+          return React.createElement(
+            'li',
+            {key: i},
+            item
+          );
+        })
+      ),
+      React.createElement(
+        NewItemForm,
+        {onAdd: this._onAdd}
+      )
     );
   }
 });
 
 ReactDOM.render(
-  React.createElement(ListOfNumbers, {max: 10}),
+  React.createElement(List, {}),
   container
 );
